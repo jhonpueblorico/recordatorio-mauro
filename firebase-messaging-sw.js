5db793d82148a982b994e6ae7bff79b9ddb567b3
Tu Nombre <tu-correo@example.com>
2025-04-05 11:23:04 -0400

    Agregando Service Worker para notificaciones

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCnt-WgeJL9po_2UvwYZ94m9kIySjL2jm4",
+  authDomain: "recordatorio-mauro.firebaseapp.com",
+  projectId: "recordatorio-mauro",
+  storageBucket: "recordatorio-mauro.firebasestorage.app",
+  messagingSenderId: "178317275132",
+  appId: "1:178317275132:web:3c228cd26effa1f0064c79"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png" // Podés cambiar esto si querés otro ícono
+  });
+});
