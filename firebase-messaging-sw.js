457f4dbef5d0855a38ea9b8c4c5b315f3629bcfc
jhonpueblorico <maocetun44@hotmail.com>
2025-04-06 14:28:37 -0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyCnt-WgeJL9po_2UvwYZ94m9kIySjL2jm4",
   authDomain: "recordatorio-mauro.firebaseapp.com",
   projectId: "recordatorio-mauro",
-  storageBucket: "recordatorio-mauro.firebasestorage.app",
+  storageBucket: "recordatorio-mauro.appspot.com",
   messagingSenderId: "178317275132",
   appId: "1:178317275132:web:3c228cd26effa1f0064c79"
 });
